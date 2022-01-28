5e6e083bc64d78415911518ae688878f48459bcc
Denis Hacquin <denis@marker.io>
2022-01-29 00:09:49 +0100

    abis

---
+++ src/contracts/index.js
@@ -0,0 +1,34 @@
+import gold from './Gold.json';
+// import goldContractAbi from '../../abi/Gold.json';
+// const claimsContract = require('./Claims.json');
+// const claimsContractAbi = require('../../abi/Claims.json');
+// const dustContract = require('./Dust.json');
+// const dustContractAbi = require('../../abi/Dust.json');
+// const toolsContract = require('./Tools.json');
+// const toolsContractAbi = require('../../abi/Tools.json');
+// const miningContract = require('./Mining.json');
+// const miningContractAbi = require('../../abi/Mining.json');
+
+export default {
+  gold,
+
+  // claims: {
+  //   address: claimsContract.address,
+  //   abi: claimsContractAbi,
+  // },
+
+  // dust: {
+  //   address: dustContract.address,
+  //   abi: dustContractAbi,
+  // },
+
+  // tools: {
+  //   address: toolsContract.address,
+  //   abi: toolsContractAbi,
+  // },
+
+  // mining: {
+  //   address: miningContract.address,
+  //   abi: miningContractAbi,
+  // },
+};
