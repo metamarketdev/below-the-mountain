a478f6edb3c5a25cff5a4a787e86e4337caac796
Denis Hacquin <denis@marker.io>
2022-01-29 22:59:52 +0100

    cta comp

---
+++ src/components/Cta.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    :class="`flex flex-row items-center place-content-center rounded bg-${color}-600 hover:bg-${color}-600 text-white font-medium bg-opacity-80 rounded-xl border-2 border-b-4 hover:-translate-y-0.5 active:translate-y-0 will-change-transform transition-all text-xl bg-sky-600 hover:bg-sky-500 border-gray-800 active:bg-sky-600 ${sizeClasses}`"
+    @click="$emit('make')"
+    :disabled="disabled"
+  >
+    <i v-if="icon" :class="`fas fa-${icon} mr-3`"></i>
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Cta',
+
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+
+    color: {
+      type: String,
+      default: 'sky',
+    },
+
+    size: {
+      type: String,
+      default: 'normal',
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    sizeClasses() {
+      return this.size === 'big' ? 'px-4 py-3 text-lg' : 'px-2 py-1';
+    },
+  },
+};
+</script>
