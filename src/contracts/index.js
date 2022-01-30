e3cbff8e4a70dd1c69e5232eb3262f837875d9e2
Denis Hacquin <denis@marker.io>
2022-01-30 17:19:50 +0100

    cleanup

--- src/contracts/index.js
+++ src/contracts/index.js
@@ -18,13 +18,15 @@ const contracts = {
 
 export async function callMethod(contractName, methodName, params = {}) {
   console.log('callMethod', contractName, methodName, { params });
-  // TODO: payable
+
   if (!contracts[contractName]) {
     console.error('No such contract:', contractName);
   }
+
   const contract = contracts[contractName];
 
-  // Workaround required naming for params (i.e. solidity mappings)
+  // Workaround required naming for params in moralis executeFunction
+  // i.e. solidity mappings have unnamed params
   const method = contract.abi.find((method) => method.name === methodName);
 
   method.inputs = method.inputs.map((input, i) => {
