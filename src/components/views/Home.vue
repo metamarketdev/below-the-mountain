7a2b5aad65a812b65a51bb3db5f9a98f799a5ca5
Denis Hacquin <denis@marker.io>
2022-01-31 16:31:33 +0100

    temp home

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,66 +1,76 @@
 <template>
   <div class="bg-black relative">
-    <div class="overflow-hidden relative top-0">
-      <div>
-        <kinesis-scroll
-          class="absolute top-0 w-full h-auto"
-          tag="img"
-          src="back.png"
-          :strength="0"
-          type="translate"
-        />
-        <kinesis-scroll
-          class="relative w-full h-auto"
-          tag="img"
-          src="main.png"
-          :strength="0"
-          type="translate"
-        />
-        <kinesis-scroll
-          class="absolute top-0 w-full h-auto"
-          tag="img"
-          src="top.png"
-          :strength="0"
-          type="scale"
-        />
+    <!-- <div class="relative overflow-hidden">
+      <scroll-y-parallax :speed="100">
+        <img src="main.jpg" />
+      </scroll-y-parallax>
 
-        <kinesis-scroll
-          class="absolute top-0 w-full h-auto"
-          tag="img"
-          src="front.png"
-          :strength="0"
-          type="translate"
-          :originY="0"
-          axis="y"
-        />
+      <scroll-y-parallax :speed="100">
+        <img src="front.png" />
+      </scroll-y-parallax>
+    </div> -->
+
+    <kinesis-container event="scroll" class="relative overflow-hidden">
+      <kinesis-element
+        class="relative w-full h-auto"
+        tag="img"
+        src="main.jpg"
+        :strength="0"
+        axis="y"
+        type="translate"
+      />
+      <!-- :originY="0"
+        :maxY="1000"
+        :minY="500"
+        transformOrigin="top" -->
+      <kinesis-element
+        class="absolute top-0 w-full h-auto"
+        tag="img"
+        src="top.png"
+        axis="y"
+        :strength="0"
+        type="scale"
+        :originY="0"
+        :transformOrigin="0"
+      />
+      <kinesis-element
+        class="absolute top-0 w-full h-auto"
+        tag="img"
+        src="front.png"
+        :strength="0"
+        type="translate"
+        :originY="0"
+        axis="y"
+      />
+
+      <div class="flex flex-row justify-center absolute top-5 w-full">
+        <!-- <img src="logo_1024.png" alt="BelowTheMountain" style="width: 33%; max-width: 512px" /> -->
       </div>
 
-      <div class="absolute top-30 right-10 p-10">
-        <div class="font-title text-6xl text-right">
+      <div class="absolute top-1/3 right-1/4">
+        <img src="logo_1024.png" alt="BelowTheMountain" width="400" />
+
+        <!-- <div class="font-title text-6xl text-right">
           COLLECT. CRAFT.
           <br />
           TRADE. EARN.
-        </div>
+        </div> -->
 
-        <router-link
+        <Butt
           to="/app"
-          class="py-2 px-4 text-center rounded-xl border-2 border-b-4 hover:-translate-y-0.5 active:translate-y-0 will-change-transform transition-all text-xl font-bold bg-sky-600 hover:bg-sky-500 border-gray-800 active:bg-sky-600"
+          class="mt-4 py-2 px-4 text-center rounded-xl border-2 border-b-4 hover:-translate-y-0.5 active:translate-y-0 will-change-transform transition-all text-xl font-bold bg-sky-600 hover:bg-sky-500 border-gray-800 active:bg-sky-600"
         >
           TRY DEMO
-        </router-link>
+        </Butt>
       </div>
-    </div>
+    </kinesis-container>
 
-    <div class="font-title text-6xl text-right my-12">
-      COLLECT. CRAFT. TRADE. EARN.
-
-      <router-link
-        to="/app"
-        class="py-2 px-4 text-center rounded-xl border-2 border-b-4 hover:-translate-y-0.5 active:translate-y-0 will-change-transform transition-all text-xl font-bold bg-sky-600 hover:bg-sky-500 border-gray-800 active:bg-sky-600"
-      >
-        TRY DEMO
-      </router-link>
-    </div>
+    <div class="p-10 my-20">s</div>
+    <div class="p-10 my-20">s</div>
+    <div class="p-10 my-20">s</div>
+    <div class="p-10 my-20">s</div>
+    <div class="p-10 my-20">s</div>
+    <div class="p-10 my-20">s</div>
   </div>
 </template>
 
